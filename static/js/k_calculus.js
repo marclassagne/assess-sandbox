3ff0350bf68fb5b075419109d7c5331ec612963a
marclassagne <marc.lassagne@ensam.eu>
2020-06-03 16:32:56 +0200

    Update k_calculus.js

--- static/js/k_calculus.js
+++ static/js/k_calculus.js
@@ -906,7 +906,7 @@ function list(){
 					}), function (data2) {
 						
 						$('#charts_' + _i).append('<div>' + data2 + '</div>');
-						
+						$('#test').append(data[num]);
 						for (var key in data[num]) {
 							
 							var functions = "";
